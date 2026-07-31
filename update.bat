c38cabeb5bc10705e41524319c28314524ec7d39
Vikas Sharma <vikasshandlya124@gmail.com>
2026-07-31 23:53:48 +0530

    Website updated

--- update.bat
+++ update.bat
@@ -5,14 +5,25 @@ echo =====================================
 echo     Uploading Website to GitHub...
 echo =====================================
 
+git pull origin main
+
 git add .
 git commit -m "Website updated"
+
 git push origin main
 
-echo.
-echo =====================================
-echo      Upload Successful!
-echo Refresh website after 1-2 minutes.
-echo =====================================
+if %errorlevel%==0 (
+    echo.
+    echo =====================================
+    echo     Upload Successful!
+    echo Refresh website after 1-2 minutes.
+    echo =====================================
+) else (
+    echo.
+    echo =====================================
+    echo     Upload Failed!
+    echo Check the error above.
+    echo =====================================
+)
 
 pause
